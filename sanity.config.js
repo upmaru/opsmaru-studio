bf76b7b55366bf8536c9bfb6b05082fa359b9f1d
Zack Siri <me@zacksiri.dev>
2024-11-15 14:11:43 +0700

    add workspace and duplicator

--- sanity.config.js
+++ sanity.config.js
@@ -5,17 +5,46 @@ import {schemaTypes} from './schemaTypes'
 import {media} from 'sanity-plugin-media'
 import {markdownSchema} from 'sanity-plugin-markdown'
 import {muxInput} from 'sanity-plugin-mux-input'
+import {crossDatasetDuplicator} from '@sanity/cross-dataset-duplicator'
 
-export default defineConfig({
-  name: 'default',
-  title: 'Opsmaru',
-
+export default defineConfig([{
+  name: 'Live',
+  title: 'Live',
   projectId: 'gyrrgtvc',
   dataset: 'production',
+  basePath: '/live',
 
-  plugins: [structureTool(), visionTool(), media(), markdownSchema(), muxInput()],
+  plugins: [
+    structureTool(), 
+    visionTool(), 
+    media(), 
+    markdownSchema(), 
+    muxInput(),
+    crossDatasetDuplicator({
+      types: ['page', 'post', 'product', 'postCategory', 'person'],
+      tool: true
+    })
+  ],
 
   schema: {
     types: schemaTypes,
   },
-})
+}, {
+  name: 'Sandbox',
+  title: 'Sandbox',
+  projectId: 'gyrrgtvc',
+  dataset: 'staging',
+  basePath: '/sandbox',
+
+  plugins: [
+    structureTool(), 
+    visionTool(), 
+    media(), 
+    markdownSchema(), 
+    muxInput()
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+}])
