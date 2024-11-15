aef79bc0e48bee1a12dae6e540d23798ac554185
Zack Siri <me@zacksiri.dev>
2024-11-15 18:17:57 +0700

    Add tech type

---
+++ schemaTypes/techType.js
@@ -0,0 +1,55 @@
+import {defineField, defineType} from 'sanity'
+
+export const tech = defineType({
+  type: 'document',
+  name: 'tech',
+  title: 'Tech',
+  fields: [
+    defineField({
+      type: 'image',
+      name: 'logo',
+      title: 'Logo',
+      description: 'The logo image of the tech',
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+        },
+      ],
+    }),
+    defineField({
+      type: 'string',
+      name: 'title',
+      title: 'Title',
+      description: 'The title of the tech',
+      validation: (e) => e.required(),
+    }),
+    defineField({
+      type: 'slug',
+      name: 'slug',
+      title: 'Slug',
+      description: 'Url friendly name',
+      validation: (e) => e.required(),
+      options: {
+        source: 'title',
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: (input) => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
+      },
+    }),
+    defineField({
+      type: 'string',
+      name: 'type',
+      title: 'Tech Type',
+      validation: (e) => e.required(),
+      options: {
+        list: [
+          {title: 'Language', value: 'language'},
+          {title: 'Framework', value: 'framework'},
+          {title: 'Database', value: 'database'},
+          {title: 'Cloud', value: 'cloud'},
+        ],
+      },
+    }),
+  ],
+})
