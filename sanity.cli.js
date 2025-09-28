660462ff219649b5cec91bf618b267716aaf170b
Zack Siri <me@zacksiri.dev>
2025-09-28 20:34:08 +0700

    update sanity studio

--- sanity.cli.js
+++ sanity.cli.js
@@ -10,5 +10,5 @@ export default defineCliConfig({
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
-  autoUpdates: true,
+  deployment: {autoUpdates: true},
 })
